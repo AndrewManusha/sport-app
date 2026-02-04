811e74e229e3810b4f0046c8feb513050d49c7dd
Dushesa <andrejmanusa2@gmail.com>
2026-02-04 19:51:18 +0100

    базовые файлы

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Основные настройки -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+</head>
+<body>
+    <header id="header">
+        <nav class="header-inner">
+
+        </nav>
+    </header>
+    
+    @yield('ad')
+    
+    <main>
+        @yield('content')
+    </main>
+    
+    <footer>
+        <div class="footer-inner">
+
+        </div>
+    </footer>
+    
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+    @yield('scripts')
+</body>
+</html>
